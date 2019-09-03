585e8c5ffde2a8bc537ec0941eadb3f69b1d8ca1
Karl Hornlund <karlhornlund@gmail.com>
2019-09-03 16:10:06 +0930

    First cut adding self attention to MBConvBlock

--- efficientnet_pytorch/model.py
+++ efficientnet_pytorch/model.py
@@ -13,6 +13,44 @@ from .utils import (
     load_pretrained_weights,
 )
 
+
+class SimpleSelfAttention(nn.Module):
+
+    def __init__(self, n_in, ks=1, sym=False):
+        super().__init__()
+        self.conv = nn.Conv1d(n_in, n_in, ks, padding=ks // 2, bias=False)
+        self.gamma = nn.Parameter(torch.tensor([0.]))
+        self.sym = sym
+        self.n_in = n_in
+
+    def forward(self, x):
+        if self.sym:
+            # symmetry hack by https://github.com/mgrankin
+            c = self.conv.weight.view(self.n_in, self.n_in)
+            c = (c + c.t()) / 2
+            self.conv.weight = c.view(self.n_in, self.n_in, 1)
+
+        size = x.size()
+        x = x.view(*size[:2], -1)   # (C,N)
+
+        # changed the order of mutiplication to avoid O(N^2) complexity
+        # (x*xT)*(W*x) instead of (x*(xT*(W*x)))
+        convx = self.conv(x)   # (C,C) * (C,N) = (C,N)   => O(NC^2)
+        xxT = torch.bmm(x, x.permute(0, 2, 1).contiguous())   # (C,N) * (N,C) = (C,C)   => O(NC^2)
+        o = torch.bmm(xxT, convx)   # (C,C) * (C,N) = (C,N)   => O(NC^2)
+        o = self.gamma * o + x
+        return o.view(*size).contiguous()
+
+
+def conv1d(ni, no, ks=1, stride=1, padding=0, bias=False):
+    "Create and initialize a `nn.Conv1d` layer with spectral normalization."
+    conv = nn.Conv1d(ni, no, ks, stride=stride, padding=padding, bias=bias)
+    nn.init.kaiming_normal_(conv.weight)
+    if bias:
+        conv.bias.data.zero_()
+    return nn.utils.spectral_norm(conv)
+
+
 class MBConvBlock(nn.Module):
     """
     Mobile Inverted Residual Bottleneck Block
@@ -61,6 +99,7 @@ class MBConvBlock(nn.Module):
         final_oup = self._block_args.output_filters
         self._project_conv = Conv2d(in_channels=oup, out_channels=final_oup, kernel_size=1, bias=False)
         self._bn2 = nn.BatchNorm2d(num_features=final_oup, momentum=self._bn_mom, eps=self._bn_eps)
+        # self._sa = SimpleSelfAttention(final_oup)
 
     def forward(self, inputs, drop_connect_rate=None):
         """
@@ -89,6 +128,7 @@ class MBConvBlock(nn.Module):
             if drop_connect_rate:
                 x = drop_connect(x, p=drop_connect_rate, training=self.training)
             x = x + inputs  # skip connection
+        # return self._sa(x)
         return x
 
 
